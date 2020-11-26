0c92b08351376923e42d133fd646cb98be958b50
sensitiveMix <m18205253820@163.com>
2020-11-26 17:10:16 +0800

    chore: adjust code quarlity

--- src/index.jsx
+++ src/index.jsx
@@ -1,53 +1,53 @@
-// import {observable,action} from 'mobx';
-// import React,{Component} from 'react';
+// import { observable, action } from 'mobx';
+// import React,{ Component } from 'react';
 // import ReactDom from 'react-dom';
-// import PropTypes from 'prop-types';
-// import { observer, PropTypes as ObservablePropTypes} from 'mobx-react'
-//
-//
-// class  Store  {
-//    @observable cache = {
-//         queue:[]
-//    }
-//    @action.bound refresh(){
-//        this.cache.queue.push(1);
-//    }
+// // import PropTypes from 'prop-types';
+// import { observer, PropTypes as ObservablePropTypes } from 'mobx-react'
+
+
+// class Store {
+//     @observable cache = {
+//         queue: []
+//     }
+
+//     @action.bound refresh () {
+//         this.cache.queue.push('1')
+//     }
 // }
-//
+
+
+// // 实例化 store
 // const store = new Store();
-//
+
 // @observer
 // class Bar extends Component{
 //     static propTypes = {
-//         queue: ObservablePropTypes.observableArray
-//     };
-//      render(){
-//          const queue = this.props.queue;
-//
-//          return <span>{queue.length}</span>
-//      }
-//
-// }
-//
-// class Foo  extends Component{
-//     static propTypes = {
-//         cache:ObservablePropTypes.observableObject
-//     };
-//
-//     render(){
-//         console.log('hshhs');
-//
-//         const cache = this.props.cache;
-//         return <div><button onClick={this.props.refresh}>refresh</button><Bar queue={cache.queue}>hhh</Bar></div>
+//         queue: ObservablePropTypes.arrayOrObservableArray
+//     }
+
+//     render () {
+//         const queue = this.props.queue;
+//         return <span>{queue.length}</span>
 //     }
 // }
-//
-//
-// ReactDom.render(<Foo cache={store.cache} refresh={store.refresh}/>,document.querySelector('#root'));
-//
-//
-//
-//
+
+// class Foo extends Component{
+//     static propTypes = {
+//         cache: ObservablePropTypes.observableObject
+//     };
+
+//     render () {
+//         const cache = this.props.cache;
+//         return <div><button onClick={this.props.refresh}>refresh</button><Bar queue={cache.queue}></Bar></div>
+//     }
+// }
+
+
+// ReactDom.render(<Foo cache={store.cache} refresh={store.refresh}/>, document.querySelector('#root'));
+
+
+
+
 
 
 
